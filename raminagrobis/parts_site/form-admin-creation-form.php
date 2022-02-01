cdff355d5fb9787f37630884335e7a6af114d1af
Théotime Caillé <93726250+Ashriga@users.noreply.github.com>
2022-02-01 13:39:07 +0100

    Corruption corrigée
    Une corruption de fichier que j'ai du résoudre car ça faisait planter tout ce qu'on avait faire. Je ne sais pas comment ça se fait que ça soit autant modifié sans que je n'ai rien fait rien pour, tout comme vous par ailleurs. Besoin d'une réponse fast ?

--- raminagrobis/parts_site/form-admin-creation-form.php
+++ raminagrobis/parts_site/form-admin-creation-form.php
@@ -11,169 +11,41 @@ include "header.php";
 ?>
 
 <!-- Modifier le form pour que ce soit u qui crée un event. -->
-<<<<<<< HEAD
+
 <h2 class="title-creation-form">Formulaire de création d'Évenement.</h2>
 <form class="formulaire-creation" action="#" method="post">
+
     <div class="div-input-label">
         <label for="image">Insérez une image :</label>
         <input type="file" name="image" id="image">
     </div>
+
     <div class="div-input-label">
         <label for="title">Entrez un titre :</label>
         <input type="text" name="title" id="title">
     </div>
+
     <div class="div-input-label">
         <label for="desc">Entrez une description :</label>
         <textarea name="desc" id="desc" rows="5" cols="33">  </textarea>
     </div>
+
     <div class="div-input-label">
         <label for="colors">Choisissez une couleur :</label>
         <select name="colors" id="colors"></select>
     </div>
-    <!--  <div class="div-input-label">
-              <label for="name">Nom : </label>
-              <input type="text" name="name" id="name">
-      </div>
 
-      <div class="div-input-label">
-              <label for="firstname">Prénom : </label>
-              <input type="text" name="firstname" id="firstname">
-      </div>
+    <div class="div-input-label">
+        <label for="secteur">Secteur : </label>
+        <select name="secteur" id="secteur">
+        <!-- Faire une boucle php forEach pour les options de secteur. -->
+        </select>
+    </div>
 
-      <div class="div-input-label">
-              <label for="email">Email : </label>
-              <input type="email" name="email" id="email">
-      </div>
 
-      <div class="div-input-label">
-              <label for="civilite">Civilité : </label>
-              <select name="civilite" id="civilite">
-              <option value="Homme"></option>
-              <option value="Femme"></option>
-              <option value="Autre"></option>
-              </select>
-      </div>
-
-      <div class="div-input-label">
-              <label for="accompagnants">Nombre d'accompagnants : </label>
-              <input type="number" name="accompagnants" id="accompagnants">
-      </div>
-
-      <div class="div-input-label">
-              <label for="mobile-phone">Téléphone portable : </label>
-              <input type="number" name="mobile-phone" id="mobile-phone">
-      </div>
-
-      <div class="div-input-label">
-              <label for="fixe-phone">Téléphone fixe : </label>
-              <input type="number" name="fixe-phone" id="fixe-phone">
-      </div> -->
-
-      <div class="div-input-label">
-              <label for="secteur">Secteur : </label>
-              <select name="secteur" id="secteur">
-                      <!-- Faire une boucle php forEach pour les options de secteur. -->
-                </select>
-        </div>
-<!--
-        <div class="div-input-label">
-=======
-
-<form class="formulaire-creation" action="#" method="post">
-        <div class="div-input-label"> 
-                <label for="name">Nom : </label>
-                <input type="text" name="name" id="name">
-        </div>
-
-        <div class="div-input-label"> 
-                <label for="firstname">Prénom : </label>
-                <input type="text" name="firstname" id="firstname">
-        </div>
-
-        <div class="div-input-label"> 
-                <label for="email">Email : </label>
-                <input type="email" name="email" id="email">
-        </div>
-
-        <div class="div-input-label"> 
-                <label for="civilite">Civilité : </label>
-                <select name="civilite" id="civilite">
-                <option value="Homme"></option>
-                <option value="Femme"></option>
-                <option value="Autre"></option>
-                </select>
-        </div>
-
-        <div class="div-input-label"> 
-                <label for="accompagnants">Nombre d'accompagnants : </label>
-                <input type="number" name="accompagnants" id="accompagnants">
-        </div>
-
-        <div class="div-input-label"> 
-                <label for="mobile-phone">Téléphone portable : </label>
-                <input type="number" name="mobile-phone" id="mobile-phone">
-        </div>
-
-        <div class="div-input-label"> 
-                <label for="fixe-phone">Téléphone fixe : </label>
-                <input type="number" name="fixe-phone" id="fixe-phone">
-        </div>
-
-        <div class="div-input-label"> 
-                <label for="secteur">Secteur : </label>
-                <select name="secteur" id="secteur">
-                        <!-- Faire une boucle php forEach pour les options de secteur. -->
-                </select>
-        </div>
-
-        <div class="div-input-label"> 
->>>>>>> 37de0556beb8851134410e172d02ffd0bb47ab37
-                <label for="entreprise">Nom de l'entreprise : </label>
-                <input type="text" name="entreprise" id="entreprise">
-        </div>
-
-<<<<<<< HEAD
-        <div class="div-input-label">
-=======
-        <div class="div-input-label"> 
->>>>>>> 37de0556beb8851134410e172d02ffd0bb47ab37
-                <label for="job">Métier : </label>
-                <input type="text" name="job" id="job">
-
-        </div>
-
-<<<<<<< HEAD
-        <div class="div-input-label">
-                <input type="checkbox" name="RGPD" id="RGPD" required>
-                <label for="RGPD">RGPD</label>
-        </div>
-
-        <div class="div-input-label">
-                <input type="checkbox" name="newsletter" id="newsletter">
-                <label for="newsletter">Newsletter</label>
-        </div>
--->
-=======
-        <div class="div-input-label"> 
-                <input type="checkbox" name="RGPD" id="RGPD" required>
-                <label for="RGPD">RGPD</label>
-        </div>
-        
-        <div class="div-input-label"> 
-                <input type="checkbox" name="newsletter" id="newsletter">
-                <label for="newsletter">Newsletter</label>
-        </div>
-
->>>>>>> 37de0556beb8851134410e172d02ffd0bb47ab37
-        <input type="submit" value="Envoyer" class="btn-submit">
 </form>
 
-
-
-<<<<<<< HEAD
 <?php
-=======
-<?php 
->>>>>>> 37de0556beb8851134410e172d02ffd0bb47ab37
-include "footer.php"
+
+        include "footer.php"
 ?>
